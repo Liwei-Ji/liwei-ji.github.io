e716d06f0d3bcf23fc634f98c8a83761515a78c5
Liwei-Ji <jpw0616@gmail.com>
2025-02-20 12:56:24 +0800

    Create script.js
    新創作品集使用

---
+++ projects/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var docHeight = document.documentElement.scrollHeight;
+    var winHeight = window.innerHeight;
+
+    var scrollPercentage = (scrollTop / (docHeight - winHeight)) * 100;
+
+    // 更新進度條寬度
+    document.querySelector('#progress-bar').style.width = scrollPercentage + '%';
+});
