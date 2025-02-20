9841ffdc83558b08380a203baaad6db50210f946
Liwei-Ji <jpw0616@gmail.com>
2025-02-20 13:01:48 +0800

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -1,10 +1,12 @@
-window.addEventListener('scroll', function() {
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    var docHeight = document.documentElement.scrollHeight;
-    var winHeight = window.innerHeight;
+        // 監聽滾動事件
+        window.addEventListener('scroll', function() {
+            var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            var docHeight = document.documentElement.scrollHeight;
+            var winHeight = window.innerHeight;
 
-    var scrollPercentage = (scrollTop / (docHeight - winHeight)) * 100;
+            var scrollPercentage = (scrollTop / (docHeight - winHeight)) * 100;
 
-    // 更新進度條寬度
-    document.querySelector('#progress-bar').style.width = scrollPercentage + '%';
-});
+        // 更新進度條寬度
+            document.querySelector('#progress-bar::before').style.width = scrollPercentage + '%';
+        });
+
